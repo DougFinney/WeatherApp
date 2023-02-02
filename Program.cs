e7a8a6716e647181b56afe0286f5d1baafca2c2c
Doug Finney <dugfiny@gmail.com>
2023-02-02 14:42:40 -0600

    Weaher By Zip

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
 ﻿
+using Newtonsoft.Json.Linq;
+using WeatherApp;
+
+string key = File.ReadAllText("appsettings.json");
+string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+Console.WriteLine("Please enter the zipcode");
+string zipCode = Console.ReadLine();
+
+string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIkey}&units=imperial";
+var temp = Weather.GetTemp(apiCall);
+
+Console.WriteLine();
+Console.WriteLine($"It is cirrently {Weather.GetTemp(apiCall)} degrees F in that location.");
---
+++ Weather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class Weather
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JsonObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+        }
+    }
+}
